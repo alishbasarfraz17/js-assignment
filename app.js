95d9354eaf2300568c253491cb5b71c09e07fdb8
Alishba Sarfraz <154826045+alishbasarfraz17@users.noreply.github.com>
2024-07-05 17:50:10 +0500

    my js work

---
+++ app.js
@@ -0,0 +1,47 @@
+
+// CALCULATOR MAKING
+
+// function calculator(){
+//     var num1 = +prompt("Enter the num1?")
+//     var operator = +prompt("Enter the operator i.e +,-,*,/")
+//     var num2 = +prompt("Enter the num2?")
+//  if(operator==="+"){
+//     return num1+num2
+//  } else if(operator==="-"){
+//     return num1-num2
+//  }
+//  else if(operator==="*"){
+//     return num1*num2
+//  }
+// else if(operator==="/"){
+//     return num1/num2
+// }
+// else{
+//     return ("invalid operator")
+// }
+
+// }
+// alert(calculator())
+
+
+
+            //    FACTORIAL
+
+// function factorial(){
+//     var num = parseInt(prompt("Enter a number?"))
+
+//     if(num<0){
+//         alert(
+//          "Error : Factorial is not defined for negative numbers"   
+//         )
+//     }else{
+
+//         var result = 1;
+//         for(var i=1 ; i <= num ; i++){
+//             result*=i
+//         }
+//         alert("The factorial of "+ num +  "is"  + result )
+//     }
+
+// }
+// factorial()
